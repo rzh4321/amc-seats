0cae7d511fea8aff83f3a0908387f0e546e0f164
rzh4321 <rzh4321@gmail.com>
2025-02-12 23:36:02 -0500

    added more chrome options

--- scraper/check_seat.py
+++ scraper/check_seat.py
@@ -38,6 +38,15 @@ def create_driver():
     chrome_options.add_experimental_option('useAutomationExtension', False)
 
 
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--shm-size=2gb')
+
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--disable-infobars')
+
+
     
     return webdriver.Chrome(options=chrome_options)
 
